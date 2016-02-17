xenserver/xscontainer

commit a12af1990a2d2caa625905e199db4e79e7c8107a

Read(1024) instead of Read(1) in monitor
This should save some CPU time.

Signed-off-by: Robert Breker <robert.breker@citrix.com>

src/xscontainer/remote_helper/ssh.py
@@ -143,8 +143,8 @@ def execute_ssh(session, vmuuid, cmd, stdin_input=None):
             client.close()
 
 
-def execute_docker_listen_charbychar(session, vmuuid, request,
-                                     stop_monitoring_request):
+def execute_docker_data_listen(session, vmuuid, request,
+                               stop_monitoring_request):
     ssh_client = prepare_ssh_client(session, vmuuid)
     try:
         cmd = prepare_request_cmd()
@@ -163,11 +163,10 @@ def execute_docker_listen_charbychar(session, vmuuid, request,
             if not rlist:
                 continue
             try:
-                # @todo: should read more than one char at once
-                character = stdout.read(1)
-                if character == "":
+                read_data = stdout.read(1024)
+                if read_data == "":
                     break
-                yield character
+                yield read_data
             except IOError, exception:
                 log.info("IOError")
                 if exception[0] not in (errno.EAGAIN, errno.EINTR):

src/xscontainer/remote_helper/__init__.py
@@ -27,26 +27,27 @@ def execute_docker_event_listen(session, vmuuid, stoprequest):
     openbrackets = 0
     request = "GET /events HTTP/1.0\r\n\r\n"
     connector = _get_connector(session, vmuuid)
-    for character in connector.execute_docker_listen_charbychar(session,
-                                                                vmuuid,
-                                                                request,
-                                                                stoprequest):
-        data = data + character
-        if (not skippedheader and character == "\n" and
-                len(data) >= 4 and data[-4:] == "\r\n\r\n"):
-            data = ""
-            skippedheader = True
-        elif character == '{':
-            openbrackets = openbrackets + 1
-        elif character == '}':
-            openbrackets = openbrackets - 1
-            if openbrackets == 0:
-                event = simplejson.loads(data)
-                yield event
+    for read_data in connector.execute_docker_data_listen(session,
+                                                          vmuuid,
+                                                          request,
+                                                          stoprequest):
+        for character in read_data:
+            data = data + character
+            if (not skippedheader and character == "\n" and
+                    len(data) >= 4 and data[-4:] == "\r\n\r\n"):
                 data = ""
-    if len(data) >= 2048:
-        raise util.XSContainerException('__monitor_vm_events' +
-                                        'is full')
+                skippedheader = True
+            elif character == '{':
+                openbrackets = openbrackets + 1
+            elif character == '}':
+                openbrackets = openbrackets - 1
+                if openbrackets == 0:
+                    event = simplejson.loads(data)
+                    yield event
+                    data = ""
+            if len(read_data) >= 2048:
+                raise util.XSContainerException('execute_docker_event_listen' +
+                                                'is full')
 
 
 def determine_error_cause(session, vmuuid):

src/xscontainer/remote_helper/tls.py
@@ -54,8 +54,8 @@ def execute_docker(session, vm_uuid, request):
     return result
 
 
-def execute_docker_listen_charbychar(session, vm_uuid, request,
-                                     stop_monitoring_request):
+def execute_docker_data_listen(session, vm_uuid, request,
+                               stop_monitoring_request):
     host = api_helper.get_suitable_vm_ip(session, vm_uuid, DOCKER_TLS_PORT)
     log.info("tls.execute_docker_listen_charbychar for VM %s, via %s"
              % (vm_uuid, host))
@@ -71,11 +71,10 @@ def execute_docker_listen_charbychar(session, vm_uuid, request,
             if not rlist:
                 continue
             try:
-                # @todo: should read more than one char at once
-                character = asocket.recv(1)
-                if character == "":
+                read_data = asocket.recv(1024)
+                if read_data == "":
                     break
-                yield character
+                yield read_data
             except IOError, exception:
                 if exception[0] not in (errno.EAGAIN, errno.EINTR):
                     raise

commit ffd7444391d61855f059296a88a928f65310a901

Fix SSH events broken in a12af19
The improvement doesn't appear to work for SSH as expected.
It will need some more work, that will be done separately.

Signed-off-by: Robert Breker <robert.breker@citrix.com>

src/xscontainer/remote_helper/ssh.py
@@ -163,7 +163,7 @@ def execute_docker_data_listen(session, vmuuid, request,
             if not rlist:
                 continue
             try:
-                read_data = stdout.read(1024)
+                read_data = stdout.read(1)
                 if read_data == "":
                     break
                 yield read_data